d06eb2c45c680376eab06f74f2fbcfb72b5e766f
Jewel Carrier-Davis <jujicb@gmail.com>
2022-07-01 22:05:37 -0400

    commit tree 7cf9b843da0e02eae3b73aa04d315662f12a6e80

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (array, string) {
+    const result = array.filter(driver => driver.toLowerCase() === string.toLowerCase())
+    return result
+}
+
+function fuzzyMatch (array, string) {
+    const result = array.filter(driver => driver.split("").slice(0, string.length).join("") === string);
+   
+    return result
+}
+
+function matchName (array, string) {
+    const result = array.filter(driver => driver["name"] === string)
+
+    return result
+}
